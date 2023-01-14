09de99a6dfebefa375a1bbe26b3495cf070b70c6
katrrrina <eka1308@gmail.com>
2023-01-14 21:14:43 +0300

    add input others

--- js/animation.js
+++ js/animation.js
@@ -24,4 +24,25 @@ function initIncreaseNumberAnimation() {
 
 initIncreaseNumberAnimation();
 
-
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+ 
+    formContainer.appendChild(input);
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+  }
+ 
+  if (event.target.value !== 'other') {
+    //  удаляем ранее созданный элемент с классом form__other-input
+    const otherInput = document.querySelector('.form__other-input');
+    if (event.target.value !== 'other' && otherInput) { // Задание 5
+      document.querySelector('#form form').removeChild(otherInput); // Задание 4
+      }
+  }
+});
